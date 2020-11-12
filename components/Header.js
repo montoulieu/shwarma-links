49ee705eb593b2c712eb0c5291cc1374b71dfc2c
Pieter Montoulieu <pieter@montoulieu.dev>
2020-11-11 21:47:46 -0700

    Header and document updates

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="./profile-pic.jpg" className="h-24 rounded-full mx-auto mb-5 shadow-lg" />
-        <h1 className="font-bold text-3xl font-bold">shwarma</h1>
+        <h1 className="font-bold text-3xl">shwarma</h1>
         <p className="text-xl">psychedelic rock from denver, co</p>
       </div>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ function IndexPage() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      {/* FontAwesome CDN style tag */}
       <Head>
         <title>shwarma | denver, co</title>
         <meta
@@ -26,13 +25,10 @@ function IndexPage() {
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous"></link>
       </Head>
 
-      {/* Profile Pic and Title Header*/}
       <Header />
 
-      {/* List of Links generated from links.json */}
       <LinkList />
 
-      {/* Social Links and Footer Disclaimer/Credits */}
       <Footer />
     </div>
   );
