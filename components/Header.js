6c0d28762a01a77fc6b3c3dac4d55c107a9d72ac
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-10-15 13:12:08 -0600

    Added ween pre party ticket link for venmo

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,8 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="./profile-pic.jpg" className="h-24 rounded-full mx-auto mb-5 shadow-lg" />
-        <h1 className="font-bold text-3xl">shwarma</h1>
-        <p className="text-xl">psychedelic rock from denver, co</p>
+        <h1 className="font-bold text-4xl">shwarma</h1>
+        <p className="text-2xl">kebab rock from denver, co</p>
         <SocialLinks />
       </div>
     </header>
--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -2,21 +2,24 @@ import socialLinks from '../json/social-links.json'
 
 function SocialLinks() {
   return (
-    <ul className="pt-6">
-      {socialLinks.map(link => (
-        <li className="mx-3 inline" key={link.icon}>
-          <a
-            className="text-purple-400 duration-200 hover:text-purple-600"
-            href={link.url}
-            target="_blank"
-            rel="noopener"
-            alt={link.icon}
-          >
-            <i className={`fa fa-2x fa-${link.icon}`}></i>
-          </a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <p className="text-xl pt-6">links to music:</p>
+      <ul className="pt-2">
+        {socialLinks.map(link => (
+          <li className="mx-3 inline" key={link.icon}>
+            <a
+              className="text-purple-400 duration-200 hover:text-purple-600"
+              href={link.url}
+              target="_blank"
+              rel="noopener"
+              alt={link.icon}
+            >
+              <i className={`fa fa-2x fa-${link.icon}`}></i>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
